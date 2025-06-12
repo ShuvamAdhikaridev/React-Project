48dc17bdf6544a44811d2112ecf05fed182895e5
Shuvam Adhikari <shuvam.adhikari.dev@gmail.com>
2025-06-12 19:27:21 +0545

    Task-List

---
+++ employement-management-system/src/componenets/TaskList/TaskList.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+const TaskList = () => {
+  return (
+    <div
+      id="tasklist"
+      className="h-[55%] overflow-auto flex items-center justify-start gap-5 flex-nowrap flew-full py-5  mt-10"
+    >
+      <div className=" flex-shrink-0 h-full w-[400px] bg-red-400 p-5 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">high</h3>
+          <h4 className="text-sm">12-06-2025</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a Youtube VIdeo</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iste
+          excepturi labore, nemo et ab.
+        </p>
+      </div>
+      <div className=" flex-shrink-0 h-full w-[400px] bg-green-400 p-5 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">high</h3>
+          <h4 className="text-sm">12-06-2025</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a Youtube VIdeo</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iste
+          excepturi labore, nemo et ab.
+        </p>
+      </div>
+      <div className=" flex-shrink-0 h-full w-[400px] bg-yellow-400 p-5 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">high</h3>
+          <h4 className="text-sm">12-06-2025</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a Youtube VIdeo</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iste
+          excepturi labore, nemo et ab.
+        </p>
+      </div>
+      <div className=" flex-shrink-0 h-full w-[400px] bg-pink-400 p-5 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">high</h3>
+          <h4 className="text-sm">12-06-2025</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a Youtube VIdeo</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iste
+          excepturi labore, nemo et ab.
+        </p>
+      </div>
+      <div className=" flex-shrink-0 h-full w-[400px] bg-blue-400 p-5 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">high</h3>
+          <h4 className="text-sm">12-06-2025</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a Youtube VIdeo</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iste
+          excepturi labore, nemo et ab.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TaskList;
