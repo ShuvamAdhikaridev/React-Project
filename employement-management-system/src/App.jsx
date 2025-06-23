69de6b6b57b9aa7c81ac12250cff36680c7b6022
Shuvam Adhikari <shuvam.adhikari.dev@gmail.com>
2025-06-23 16:00:11 +0545

    LoggedInUser

--- employement-management-system/src/App.jsx
+++ employement-management-system/src/App.jsx
@@ -13,14 +13,29 @@ function App() {
   const [user, setUser] = useState(null);
   const authData = useContext(AuthContext);
 
+  useEffect(() => {
+    if (authData) {
+      const loggedInUser = localStorage.getItem("loggedInUser");
+
+      if (loggedInUser) {
+        setUser(loggedInUser.role);
+      }
+    }
+  }, [authData]);
+
   const handleLogin = (email, password) => {
     if (email == `admin@me.com` && password == `123`) {
       setUser(`admin`);
+      localStorage.setItem(`loggedInUser`, JSON.stringify({ role: `admin` }));
     } else if (
       authData &&
       authData.employees.find((e) => email == e.email && e.password == password)
     ) {
       setUser(`employee`);
+      localStorage.setItem(
+        `loggedInUser`,
+        JSON.stringify({ role: `employee` })
+      );
     } else {
       alert(`Invalid Credentials`);
     }
